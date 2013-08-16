845afa7edd8237013cb95f09d54a023f9e0ced10
Garrett Eidsvig <garrett@172-18-137-108.ip.hootsuitemedia.com>
2013-08-16 10:43:07 -0700

    instead of returning a response stating a duplicate websocket for uuid, we close the other socket, and start a new connection

--- src/main/scala/geidsvig/netty/socket/ws/WebSocketManager.scala
+++ src/main/scala/geidsvig/netty/socket/ws/WebSocketManager.scala
@@ -7,6 +7,7 @@ import akka.actor.actorRef2Scala
 import akka.event.LoggingAdapter
 import geidsvig.netty.rest.ChannelWithRequest
 import geidsvig.netty.rest.RestUtils
+import org.jboss.netty.handler.codec.http.websocketx.CloseWebSocketFrame
 
 trait WebSocketManagerRequirements {
   val webSocketHandlerFactory: WebSocketHandlerFactory
@@ -31,20 +32,19 @@ abstract class WebSocketManager extends RestUtils {
      */
     extractUuid(request.request) match {
       case Some(uuid) => {
+        
         // check if we have registered the handler.
-        // if yes, check if actorRef is alive.
-        // if yes. respond to client that they already have a connection and close the request.ctx.channel
+        // if yes, then tell it to close, because there is a new handler to replace it.
         // if no, then create a new handler, and send it the request
         hasRegisteredHandler(uuid) match {
-          case None => {
-            val handler = webSocketHandlerFactory.createWebSocketHandler(uuid)
-            handler ! request
-          }
+          case None => {}
           case Some(handler) => {
-            val response = createHttpResponse(HttpResponseStatus.CONFLICT, callback(request.request, "Duplicate websocket request for uuid"))
-            sendHttpResponse(request.ctx, request.request, response)
+            handler ! new CloseWebSocketFrame
           }
         }
+        
+        val newHandler = webSocketHandlerFactory.createWebSocketHandler(uuid)
+        newHandler ! request
       }
       case None => {
         val response = createHttpResponse(HttpResponseStatus.BAD_REQUEST, callback(request.request, "Missing uuid"))
